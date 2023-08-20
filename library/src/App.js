5f9df6f60e5b601233bd3cd68674460723419d09
Akash <akashchauhan087@gmail.com>
2023-08-20 07:01:22 +0530

    city selector based on country

--- library/src/App.js
+++ library/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Nav from './components/Nav';
 import Intro1 from './components/Intro1';
 import Footer from './components/Footer';
-// import Countries from './components/Countries';
+import Countries from './components/Countries';
 
 function App() {
   return (
     <>
       <Nav title="Library" />
       <Intro1 />
-      {/* <Countries /> */}
+      <Countries />
       <Footer />
     </>
   );
--- library/src/components/Countries.js
+++ library/src/components/Countries.js
@@ -1,3 +1,5 @@
+import React, {useState} from 'react'
+
 const country = [
     {
         name: "India",
@@ -14,24 +16,25 @@ const country = [
 ]
 
 function Countries() {
-
+    const [countryCapital, capitalUpdateByCountry] = useState('0');
+    const selectedCountry = (e) => {
+        capitalUpdateByCountry(e.target.value)
+    }
     return (
-        <div className="country-dropdown">
-            <select
-                onChange={(e) => {
-                    alert(e.target.value);
-                }}
-            >
+        <div className="container country-dropdown">
+            <select onChange={selectedCountry} className='mx-1'>
                 {
                     country.map((item, index) => {
                         return (
-                            <option value={item.name}>{item.name} </option>
+                            <option key={index} value={index}> {item.name} </option>
                         )
                     })
                 }
             </select>
+            <select className='mx-1'>
+                <option> {country[countryCapital].capital} </option>
+            </select>
         </div>
-
     );
 };
 
